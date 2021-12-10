d9f8d387306e71988c5e17ba56d0a879b3d68e6c
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-12-10 13:37:18 -0600

    setup sql

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
    console.log(`API server now on port ${PORT}!`);
